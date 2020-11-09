9cc97b248a01f19e6ac27b52930e0efe0f02ab76
AwesomeNicknameEver <katerina.muskova@gmail.com>
2020-11-09 18:10:57 +0100

    clean data

--- download.py
+++ download.py
@@ -42,17 +42,19 @@ class DataDownloader():
     }
 
     date_rows = [3]
-    float_rows = [47, 48, 49, 50]
-    string_rows = [51, 52, 54, 58, 59, 62]
+    float_rows = [45, 46, 47, 48, 49, 50]
+    string_rows = [51, 52, 53, 54, 55, 58, 59, 62, 64]
+    int_rows = [0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
+                29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 56, 57, 60, 61, 63]
 
-    column_names = ['reg', 'p1', 'p36', 'p37', 'p2a', 'weekday(p2a)', 'p2b', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11',
+    column_names = ['p1', 'p36', 'p37', 'p2a', 'weekday(p2a)', 'p2b', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11',
                     'p12',
                     'p13a', 'p13b', 'p13c', 'p14', 'p15', 'p16', 'p17', 'p18', 'p19', 'p20', 'p21', 'p22', 'p23',
                     'p24', 'p27', 'p28', 'p34', 'p35', 'p39', 'p44', 'p45a', 'p47', 'p48a', 'p49', 'p50a', 'p50b',
                     'p51', 'p52', 'p53', 'p55a', 'p57', 'p58', 'a', 'b', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                    'n', 'o', 'p', 'q', 'r', 's', 't', 'p5a']
+                    'n', 'o', 'p', 'q', 'r', 's', 't', 'p5a', 'reg']
 
-    column_description = ['reg', 'id', 'druh pozemni komunikace', 'cislo pozemni komunikace', 'rok mesic den',
+    column_description = ['id', 'druh pozemni komunikace', 'cislo pozemni komunikace', 'rok mesic den',
                           'den v tydnu',
                           'cas', 'druh nehody', 'druh strazky s jedoucim vozidlem', 'druh pevne prekasky',
                           'charakter nehody', 'zavineni nehody', 'alkohol vinika', 'hlavni pricina', 'usmrceno',
@@ -64,7 +66,7 @@ class DataDownloader():
                           'charakter vozidla', 'smyk', 'vozidlo po nehode', 'unik hmot',
                           'smer jizdy nebo pozastaveni voz', 'skoda na vozidle', 'kat ridice', 'stav ridice',
                           'vnejsi ovlivneni', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',
-                          'lokalita nehody'
+                          'lokalita nehody', 'reg'
                           ]
 
     # 54
@@ -79,8 +81,6 @@ class DataDownloader():
     # 62
     # ('', 'Pomalý', 'Velmirychlý', 'Rychlý', 'Kolektor', 'Odbočovacívpravo', 'Připojovacívpravo', 'Připojovacívlevo', 'Propomalávozidla', 'Odbočovacívlevo', 'Řadicí', 'Řadicívpravo')
 
-    origo = tuple()
-
     def __init__(self, url="https://ehw.fit.vutbr.cz/izv/", folder="ataf", cache_filename="ata_{}.pkl.gz"):
         """
         :param url: from which URL will be data downloaded
@@ -93,7 +93,7 @@ class DataDownloader():
         self.cache_filename = cache_filename
         self.cached_regions = dict()
 
-        self.original_2 = tuple()
+        self.original_2 = dict()
 
     def correct_irregular_file_names(self, file_name: str) -> str:
         if file_name.startswith('data-gis'):
@@ -175,13 +175,44 @@ class DataDownloader():
 
                     with zf.open(region_file, 'r') as csv_f:
                         csv_reader = csv.reader(TextIOWrapper(csv_f, 'windows-1250'), delimiter=';', quotechar='"')
-                        one_zip_list = list([region] + row for row in csv_reader)
+                        one_zip_list = list(self.clean_row_data(row, region) for row in csv_reader)
                         data_list += one_zip_list
 
             except zipfile.BadZipFile as e:
                 logging.warning(f'zip file {d_zip} is broken.')
 
         return data_list
+
+    def clean_numpy_data(self, data_list):
+        # NAT je None
+        data = np.array(data_list).T
+
+        d_time = np.array(data[self.date_rows], dtype=np.datetime64)
+        d_str = np.array(data[self.string_rows], dtype=str)
+
+        data_floats = np.where(data[self.float_rows] == "", -1, data[self.float_rows])
+        data_list = data_floats * 100
+        d_float = np.array(data_floats, dtype=float)
+
+        data_ints = np.where(data[self.int_rows] == "", -1, data[self.int_rows])
+        d_int = np.array(data_ints, dtype=int)
+
+        return print(np.array((*d_time, *d_str, *d_float, *d_int), dtype=object).T)
+
+    def clean_row_data(self, row: list, reg):
+        row[3] = np.datetime64(row[3])
+        try:
+            row[34] = int(row[34])
+        except ValueError as e:
+            row[34] = -1
+
+        for id in self.float_rows:
+            try:
+                row[id] = row[id].replace(',', '.')
+            except ValueError as e:
+                row[id] = None
+        row.append(reg)
+        return row
 
     def parse_region_data(self, region):
         """
@@ -204,7 +235,7 @@ class DataDownloader():
 
         # todo clean
         data_list = self.get_row_list(region, region_file, downloaded_zips)
-        data_numpy = np.array(data_list)
+        data_numpy = self.clean_numpy_data(data_list)
 
         return tuple([self.column_names, data_numpy])
 
@@ -215,11 +246,11 @@ class DataDownloader():
         :return:
         """
 
+        if regions is None:
+            regions = region_codes.keys()
+
         if not all([r in region_codes for r in regions]):
             return None
-
-        if regions is None:
-            regions = region_codes
 
         return_list = list()
         for reg in regions:
@@ -242,20 +273,51 @@ class DataDownloader():
 
         return tuple([self.column_names, *return_list])
 
-    def fnc(l):
-        print(id(l))
+    def unique_data(self, data, id=None):
+        data = data.T
+
+        if (id is None):
+            return [np.unique(data[i]) for i in range(0, len(data))]
+
+        return [np.unique(data[i]) for i in id]
 
 
 if __name__ == '__main__':
     # todo try download multiple times
     # todo comments
+
+    # print([i for i in range(0, 66) if i not in DataDownloader.string_rows and
+    #        i not in DataDownloader.float_rows and
+    #        i not in DataDownloader.date_rows])
+    #
+    # exit(-1)
+
+    # a = np.array([[1.0, 2.3, 3.5], [1.0, 2.3, 3.5]])
+    # b = a.view(dtype=int)
+    # res = np.where(b[0] > 2, 0, 10)
+    # print(res)
+    # print(a)
+    # print(b)
+    # exit(-1)
+
     d = DataDownloader()
+
+    dd = d.parse_region_data("PHA")
+    print(dd[1])
+
+    exit(-1)
+
+    # pd = d.get_list()
+    # for i in d.unique_data(pd[1], range(34, 35)):
+    #     print(i)
+    # # print(pd[1][0])
+    # exit(-1)
 
     regions = ["PHA", "JHM", "OLK"]
     if len(sys.argv) == 4:
         regions = sys.argv[1:]
 
-    l = d.get_list(regions)
+    l = d.parse_region_data(regions)
 
     print('regions:\t', regions)
     print('row number:\t', len(l[1]))
