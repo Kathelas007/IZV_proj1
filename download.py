118f2942ed02143a7a0e458ffdb6c3a59494c621
AwesomeNicknameEver <katerina.muskova@gmail.com>
2020-11-10 18:36:53 +0100

    FERTIGGGG

--- download.py
+++ download.py
@@ -42,19 +42,19 @@ class DataDownloader():
     }
 
     date_rows = [3]
-    string_rows = [51, 52, 53, 54, 55, 58, 59, 62]
+    string_rows = [51, 52, 53, 54, 55, 56, 57, 58, 59, 62]
     int_rows = [0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
-                29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 56, 57, 60, 61, 63]
+                29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 60, 61, 63]
     float_rows = [45, 46, 47, 48, 49, 50]
 
-    column_names = ['p2a', 'h', 'i', 'j', 'k', 'l', 'p', 'q', 't', 'p1', 'p36', 'p37', 'weekday(p2a)', 'p2b', 'p6',
-                    'p7', 'p8', 'p9', 'p10', 'p11', 'p12', 'p13a', 'p13b', 'p13c', 'p14', 'p15', 'p16', 'p17', 'p18',
-                    'p19', 'p20', 'p21', 'p22', 'p23', 'p24', 'p27', 'p28', 'p34', 'p35', 'p39', 'p44', 'p45a', 'p47',
-                    'p48a', 'p49', 'p50a', 'p50b', 'p51', 'p52', 'p53', 'p55a', 'p57', 'p58', 'n', 'o', 'r', 's', 'p5a',
+    column_names = ['p2a', 'h', 'i', 'j', 'k', 'l', 'n', 'o', 'p', 'q', 't', 'p1', 'p36', 'p37', 'weekday(p2a)', 'p2b',
+                    'p6', 'p7', 'p8', 'p9', 'p10', 'p11', 'p12', 'p13a', 'p13b', 'p13c', 'p14', 'p15', 'p16', 'p17',
+                    'p18', 'p19', 'p20', 'p21', 'p22', 'p23', 'p24', 'p27', 'p28', 'p34', 'p35', 'p39', 'p44', 'p45a',
+                    'p47', 'p48a', 'p49', 'p50a', 'p50b', 'p51', 'p52', 'p53', 'p55a', 'p57', 'p58', 'r', 's', 'p5a',
                     'a', 'b', 'd', 'e', 'f', 'g', 'reg']
 
-    column_description = ['rok mesic den', 'h', 'i', 'j', 'k', 'l', 'p', 'q', 't', 'id', 'druh pozemni komunikace',
-                          'cislo pozemni komunikace', 'den v tydnu', 'cas', 'druh nehody',
+    column_description = ['rok mesic den', 'h', 'i', 'j', 'k', 'l', 'n', 'o', 'p', 'q', 't', 'id',
+                          'druh pozemni komunikace', 'cislo pozemni komunikace', 'den v tydnu', 'cas', 'druh nehody',
                           'druh strazky s jedoucim vozidlem', 'druh pevne prekasky', 'charakter nehody',
                           'zavineni nehody', 'alkohol vinika', 'hlavni pricina', 'usmrceno', 'tezce zraneno',
                           'lehce zraneno', 'skoda', 'druh povrchu', 'stav povrchu', 'stav komunikace',
@@ -64,246 +64,241 @@ class DataDownloader():
                           'druh krizujici komunikace', 'druh vozidla', 'znacka mot vozidla', 'rok vyroby vozidla',
                           'charakter vozidla', 'smyk', 'vozidlo po nehode', 'unik hmot', 'zpusob vyprosteni',
                           'smer jizdy nebo pozastaveni voz', 'skoda na vozidle', 'kat ridice', 'stav ridice',
-                          'vnejso ovlivneni', 'n', 'o', 'r', 's', 'lokalita nehody', 'a', 'b', 'd', 'e', 'f', 'g',
-                          'reg']
+                          'vnejso ovlivneni', 'r', 's', 'lokalita nehody', 'a', 'b', 'd', 'e', 'f', 'g', 'reg']
 
+    def __init__(self, url="https://ehw.fit.vutbr.cz/izv/", folder="ataf", cache_filename="ata_{}.pkl.gz"):
+        """
+        :param url: from which URL will be data downloaded
+        :param folder: folder for tmp data
+        :param cache_filename: file name in 'folder' with data processed with get_list
+        """
 
-def __init__(self, url="https://ehw.fit.vutbr.cz/izv/", folder="ataf", cache_filename="ata_{}.pkl.gz"):
-    """
-    :param url: from which URL will be data downloaded
-    :param folder: folder for tmp data
-    :param cache_filename: file name in 'folder' with data processed with get_list
-    """
+        self.url = url
+        self.folder = folder
+        self.cache_filename = cache_filename
+        self.cached_regions = dict()
 
-    self.url = url
-    self.folder = folder
-    self.cache_filename = cache_filename
-    self.cached_regions = dict()
+        self.original_2 = dict()
 
-    self.original_2 = dict()
+    def correct_irregular_file_names(self, file_name: str) -> str:
+        """
+        Zip names at URL are irregular. This fnc fices it.
+        :param file_name: zip name
+        """
+        if file_name.startswith('data-gis'):
+            return file_name.replace('data-gis', 'datagis')
 
+        if file_name == 'datagis2016.zip':
+            return 'datagis-12-2016.zip'
 
-def correct_irregular_file_names(self, file_name: str) -> str:
-    """
-    Zip names at URL are irregular. This fnc fices it.
-    :param file_name: zip name
-    """
-    if file_name.startswith('data-gis'):
-        return file_name.replace('data-gis', 'datagis')
+        if 'rok' in file_name:
+            return file_name.replace('rok', '12')
 
-    if file_name == 'datagis2016.zip':
-        return 'datagis-12-2016.zip'
+        pattern = 'datagis-'
+        if not file_name.startswith(pattern):
+            index = len(pattern) - 1
+            return file_name[:index] + '-' + file_name[index:]
 
-    if 'rok' in file_name:
-        return file_name.replace('rok', '12')
+        return file_name
 
-    pattern = 'datagis-'
-    if not file_name.startswith(pattern):
-        index = len(pattern) - 1
-        return file_name[:index] + '-' + file_name[index:]
+    def iter_file_names(self, html_response):
+        """
+        Fnc finds html table element and iterates throught its zip file names
+        :return: current file name
+        """
 
-    return file_name
+        soup = BeautifulSoup(html_response.text, 'html.parser')
+        table_el = soup.find("table", {'class': 'table table-fluid'})
 
+        last_file = None
+        for tr_el in table_el.find_all('a', {'class': ['btn', 'btm-sm', 'btm-primary']}):
+            href_zip = tr_el['href']
+            if not href_zip.endswith('zip'): continue
 
-def iter_file_names(self, html_response):
-    """
-    Fnc finds html table element and iterates throught its zip file names
-    :return: current file name
-    """
+            corrected = self.correct_irregular_file_names(href_zip[5:])
+            if corrected.startswith('datagis-12'):
+                yield href_zip
+            else:
+                last_file = href_zip
 
-    soup = BeautifulSoup(html_response.text, 'html.parser')
-    table_el = soup.find("table", {'class': 'table table-fluid'})
+        yield last_file
 
-    last_file = None
-    for tr_el in table_el.find_all('a', {'class': ['btn', 'btm-sm', 'btm-primary']}):
-        href_zip = tr_el['href']
-        if not href_zip.endswith('zip'): continue
+    def download_file(self, file_name: str):
+        """
+        Fnc downloads 1 file if is not already downloaded
+        """
+        zip_file = path.split(file_name)[-1]
+        zip_file = self.correct_irregular_file_names(zip_file)
+        zip_file = path.join(self.folder, zip_file)
+        full_url = path.join(self.url, file_name)
 
-        corrected = self.correct_irregular_file_names(href_zip[5:])
-        if corrected.startswith('datagis-12'):
-            yield href_zip
-        else:
-            last_file = href_zip
+        if path.isfile(zip_file): return
 
-    yield last_file
+        with requests.get(full_url, stream=True) as r:
+            with open(zip_file, 'wb') as f:
+                for chunk in r:
+                    f.write(chunk)
 
+    def download_data(self):
+        """
+        Fnc downloads all files from 'URL' to 'folder'
+        """
+        pathlib.Path(self.folder).mkdir(parents=True, exist_ok=True)
 
-def download_file(self, file_name: str):
-    """
-    Fnc downloads 1 file if is not already downloaded
-    """
-    zip_file = path.split(file_name)[-1]
-    zip_file = self.correct_irregular_file_names(zip_file)
-    zip_file = path.join(self.folder, zip_file)
-    full_url = path.join(self.url, file_name)
+        with requests.Session() as s:
+            html_response = s.get(self.url, headers=self.headers)
+            for file_names in self.iter_file_names(html_response):
+                self.download_file(file_names)
 
-    if path.isfile(zip_file): return
+    def zip_row_generator(self, region, region_file, downloaded_zips):
+        """
+        Row generator. Fnc yields all rows form all zip files of specified region.
+        :param region: region code
+        :param region_file: file name with region
+        :param downloaded_zips: names of all downloaded zips
+        :return: row
+        """
+        for d_zip in downloaded_zips:
+            try:
+                with zipfile.ZipFile(d_zip) as zf:
+                    if region_file not in zf.namelist(): continue
 
-    with requests.get(full_url, stream=True) as r:
-        with open(zip_file, 'wb') as f:
-            for chunk in r:
-                f.write(chunk)
+                    with zf.open(region_file, 'r') as csv_f:
+                        csv_reader = csv.reader(TextIOWrapper(csv_f, 'windows-1250'), delimiter=';', quotechar='"')
+                        for row in csv_reader:
+                            yield self.clean_row_data(row, region)
 
+            except zipfile.BadZipFile as e:
+                logging.warning(f'zip file {d_zip} is broken.')
 
-def download_data(self):
-    """
-    Fnc downloads all files from 'URL' to 'folder'
-    """
-    pathlib.Path(self.folder).mkdir(parents=True, exist_ok=True)
+    def clean_numpy_data(self, data_list, region):
+        """
+        Fnc sets proper data type and cleans data. At the end adds region
+        :param data_list: all data from one region
+        :return: cleaned numpy array
+        """
 
-    with requests.Session() as s:
-        html_response = s.get(self.url, headers=self.headers)
-        for file_names in self.iter_file_names(html_response):
-            self.download_file(file_names)
+        data = np.array(data_list).T
 
+        # correct ONE row in whole data, that was corrupted
+        if region == "PLK":
+            spoiled_colls = range(45, 56)
+            index = np.where(data[spoiled_colls[0]] == "A:")
+            data[spoiled_colls, (*index[0])] = ""
 
-def zip_row_generator(self, region, region_file, downloaded_zips):
-    """
-    Row generator. Fnc yields all rows form all zip files of specified region.
-    :param region: region code
-    :param region_file: file name with region
-    :param downloaded_zips: names of all downloaded zips
-    :return: row
-    """
-    for d_zip in downloaded_zips:
+        d_time = np.array(data[self.date_rows], dtype=np.datetime64)
+        d_str = np.array(data[self.string_rows], dtype=str)
+
+        data_floats = np.where(data[self.float_rows] == "", -1, data[self.float_rows])
+        d_float = np.array(data_floats.astype(float) * 100, dtype=int)
+
+        data_ints = np.where(np.logical_or(data[self.int_rows] == "", data[self.int_rows] == '2423A1012423A100'), -1,
+                             data[self.int_rows])
+        d_int = np.array(data_ints, dtype=int)
+
+        region_row = np.full((len(data[0])), region)
+
+        return np.array((*d_time, *d_str, *d_int, *d_float, region_row), dtype=object).T
+
+    def clean_row_data(self, row: list, reg):
+        """
+        Basic clean on one row.
+        :return: cleaned row
+        """
+        row[3] = np.datetime64(row[3])
         try:
-            with zipfile.ZipFile(d_zip) as zf:
-                if region_file not in zf.namelist(): continue
-
-                with zf.open(region_file, 'r') as csv_f:
-                    csv_reader = csv.reader(TextIOWrapper(csv_f, 'windows-1250'), delimiter=';', quotechar='"')
-                    for row in csv_reader:
-                        yield self.clean_row_data(row, region)
-
-        except zipfile.BadZipFile as e:
-            logging.warning(f'zip file {d_zip} is broken.')
-
-
-def clean_numpy_data(self, data_list, region):
-    """
-    Fnc sets proper data type and cleans data. At the end adds region
-    :param data_list: all data from one region
-    :return: cleaned numpy array
-    """
-    data = np.array(data_list).T
-
-    d_time = np.array(data[self.date_rows], dtype=np.datetime64)
-    d_str = np.array(data[self.string_rows], dtype=str)
-
-    data_floats = np.where(data[self.float_rows] == "", -1, data[self.float_rows])
-    d_float = np.array(data_floats.astype(float) * 100, dtype=int)
-
-    data_ints = np.where(data[self.int_rows] == "", -1, data[self.int_rows])
-    d_int = np.array(data_ints, dtype=int)
-
-    region_row = np.full((len(data[0])), region)
-
-    return np.array((*d_time, *d_str, *d_int, *d_float, region_row), dtype=object).T
-
-
-def clean_row_data(self, row: list, reg):
-    """
-    Basic clean on one row.
-    :return: cleaned row
-    """
-    row[3] = np.datetime64(row[3])
-    try:
-        row[34] = int(row[34])
-    except ValueError as e:
-        row[34] = -1
-
-    for r_id in self.float_rows:
-        try:
-            row[r_id] = row[r_id].replace(',', '.')
+            row[34] = int(row[34])
         except ValueError as e:
-            row[r_id] = None
-    row.append(reg)
-    return row
+            row[34] = -1
 
+        for r_id in self.float_rows:
+            try:
+                row[r_id] = row[r_id].replace(',', '.')
+            except ValueError as e:
+                row[r_id] = None
+        return row
 
-def parse_region_data(self, region):
-    """
-    Fnc parses specified 'region' of downloaded data . If data are not downloaded, it downloads them.
-    :param region: region code (3 chars)
-    :return: tuple(list(columns names), numpy(region data))
-    """
+    def parse_region_data(self, region):
+        """
+        Fnc parses specified 'region' of downloaded data . If data are not downloaded, it downloads them.
+        :param region: region code (3 chars)
+        :return: tuple(list(columns names), numpy(region data))
+        """
 
-    if region not in region_codes.keys(): return None
+        if region not in region_codes.keys(): return None
 
-    # check downloaded zips
-    downloaded_zips = [zip_file for zip_file in os.listdir(self.folder)
-                       if zip_file.endswith('.zip')]
-    if len(downloaded_zips) == 0:
-        self.download_data()
+        # check downloaded zips
+        downloaded_zips = [zip_file for zip_file in os.listdir(self.folder)
+                           if zip_file.endswith('.zip')]
+        if len(downloaded_zips) == 0:
+            self.download_data()
 
-    # get zip names
-    region_file = region_codes[region] + '.csv'
-    region_path = path.join(self.folder, region_file)
-    downloaded_zips = [path.join(self.folder, zip_file) for zip_file in os.listdir(self.folder)
-                       if zip_file.endswith('.zip')]
+        # get zip names
+        region_file = region_codes[region] + '.csv'
+        region_path = path.join(self.folder, region_file)
+        downloaded_zips = [path.join(self.folder, zip_file) for zip_file in os.listdir(self.folder)
+                           if zip_file.endswith('.zip')]
 
-    # get all rows as numpy
-    data_list = [row for row in self.zip_row_generator(region, region_file, downloaded_zips)]
-    data_numpy = self.clean_numpy_data(data_list, region)
+        # get all rows as numpy
+        data_list = [row for row in self.zip_row_generator(region, region_file, downloaded_zips)]
+        data_numpy = self.clean_numpy_data(data_list, region)
 
-    return tuple([self.column_names, data_numpy])
+        return tuple([self.column_names, data_numpy])
 
+    def get_list(self, regs=None):
+        """
+        Fnc returns data parsed by parse_region_data. Data are cached in 'folder' and in attribute cached_regions.
+        If any of cache is available, it will be used instead of parse_region_data.
+        :param regs: list of selected regions, None means all regions
+        :return: tuple(list(columns names), numpy(region data))
+        """
 
-def get_list(self, regs=None):
-    """
-    Fnc returns data parsed by parse_region_data. Data are cached in 'folder' and in attribute cached_regions.
-    If any of cache is available, it will be used instead of parse_region_data.
-    :param regs: list of selected regions, None means all regions
-    :return: tuple(list(columns names), numpy(region data))
-    """
+        if regs is None:
+            regs = region_codes.keys()
 
-    if regs is None:
-        regs = region_codes.keys()
+        if not all([r in region_codes for r in regs]):
+            return None
 
-    if not all([r in region_codes for r in regs]):
-        return None
+        return_list = list()
+        for reg in regs:
+            reg_file = path.join(self.folder, self.cache_filename.format(reg))
 
-    return_list = list()
-    for reg in regs:
-        reg_file = path.join(self.folder, self.cache_filename.format(reg))
+            # cached in class
+            if reg in self.cached_regions.keys():
+                return_list.append(self.cached_regions[reg][1])
 
-        # cached in class
-        if reg in self.cached_regions.keys():
-            return_list.append(self.cached_regions[reg][1])
+            # cached in 'folder'
+            elif path.isfile(reg_file):
+                with gzip.open(reg_file, 'rb') as gz:
+                    pck = pickle.load(gz)
+                    return_list.append(pck)
 
-        # cached in 'folder'
-        elif path.isfile(reg_file):
-            with gzip.open(reg_file, 'rb') as gz:
-                pck = pickle.load(gz)
-                return_list.append(pck)
+            # not cached
+            else:
+                # get data
+                curr_zip_data = self.parse_region_data(reg)
+                return_list.append(curr_zip_data[1])
 
-        # not cached
-        else:
-            # get data
-            curr_zip_data = self.parse_region_data(reg)
-            return_list.append(curr_zip_data[1])
+                # cache it
+                self.cached_regions[reg] = curr_zip_data[1]
+                with gzip.open(reg_file, 'wb') as gz:
+                    pickle.dump(curr_zip_data[1], gz)
 
-            # cache it
-            self.cached_regions[reg] = curr_zip_data[1]
-            with gzip.open(reg_file, 'wb') as gz:
-                pickle.dump(curr_zip_data[1], gz)
+        return tuple([self.column_names, list([item for sublist in return_list for item in sublist])])
 
-    return tuple([self.column_names, *return_list])
+    def unique_data(self, data, ids=None):
+        """
+        Analysis function. It returns unique data in givan indexes
+        :param data: region data
+        :param ids: column ids
+        :return: unique values
+        """
+        data = data.T
 
+        if ids is None:
+            return [np.unique(data[i]) for i in range(0, len(data))]
 
-def unique_data(self, data, ids=None):
-    """
-    Analysis function. It returns unique data in givan indexes
-    :param data: region data
-    :param ids: column ids
-    :return: unique values
-    """
-    data = data.T
-
-    if ids is None:
-        return [np.unique(data[i]) for i in range(0, len(data))]
-
-    return [np.unique(data[i]) for i in ids]
+        return [np.unique(data[i]) for i in ids]
 
 
 if __name__ == '__main__':
--- get_stat.py
+++ get_stat.py
@@ -1,11 +1,80 @@
 from download import DataDownloader
+
 import argparse
-import pandas as pd
+from matplotlib import gridspec
+import matplotlib.pyplot as plt
+import numpy as np
+
+import os.path
+
+
+def process_data(data_source):
+    date_coll = np.array([row[0].year for row in data_source[1]])
+    reg_coll = np.array([row[-1] for row in data_source[1]])
+
+    df = np.array([date_coll, reg_coll])
+
+    colls = np.unique(df[0])  # years
+    rows = np.unique(df[1])  # regs
+
+    return colls, rows, np.array([[np.logical_and(df[0] == c, df[1] == r).sum() for c in colls] for r in rows])
+
+
+def annotate_bars(rects, ax, df):
+    arg_sort = np.argsort(df)
+    indexes = [0] * len(df)
+
+    for i in range(0, len(df)):
+        indexes[arg_sort[i]] = i
+
+    for i in range(0, len(rects)):
+        height = rects[i].get_height()
+        ax.annotate('{}'.format(indexes[i] + 1),
+                    xy=(rects[i].get_x() + rects[i].get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+
+def get_fig(years, regs, df):
+    fig = plt.figure(figsize=(8.27, 11.69))
+    fig.suptitle("Přehled nehodovosti v krajích za rok 2016-2020", fontsize=16, fontweight='bold')
+
+    spec = gridspec.GridSpec(ncols=2, nrows=3, figure=fig, hspace=0.4)
+    axes = list()
+
+    for i in range(0, 3):
+        for j in range(0, 2):
+            if i == 2 and j == 1: break
+            axes.append(fig.add_subplot(spec[i, j]))
+
+    for i in range(0, len(axes)):
+        ax = axes[i]
+        rects = ax.bar(regs, df[:, i], color="C1", align='center')
+
+        annotate_bars(rects, ax, df[:, i])
+
+        ax.set_title(years[i], fontsize=14, fontweight='bold')
+        ax.tick_params(axis='x', labelrotation=45)
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.set_axisbelow(True)
+        ax.grid(axis="y", linewidth=1, linestyle=":")
+        ax.margins(x=0.02)
+        ax.set_ylim(top=df.max() * 1.1)
+
+    return fig
 
 
 def plot_stat(data_source, fig_location=None, show_figure=False):
-    df = pd.DataFrame(data_source[1], columns=data_source[0])
-    print(df)
+    colls_year, rows_reg, df = process_data(data_source)
+    fig = get_fig(colls_year, rows_reg, df)
+
+    if show_figure:
+        fig.show()
+
+    if fig_location is not None:
+        fig.savefig(os.path.join(fig_location, 'stat.svg'))
 
 
 def parse_args():
@@ -20,6 +89,5 @@ def parse_args():
 if __name__ == '__main__':
     show, fig_loc = parse_args()
     d = DataDownloader()
-    data_source = d.get_list(['PHA', 'JHM'])
-
+    data_source = d.get_list()
     plot_stat(data_source, fig_loc, show)
